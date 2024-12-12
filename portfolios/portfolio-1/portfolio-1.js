078351b09441bc9f83efc72695a643affb8e291a
peaceyyy <peacetech018@gmail.com>
2024-12-12 23:05:13 +0800

    refactor carousel functionality and improve responsive styles

--- portfolios/portfolio-1/portfolio-1.js
+++ portfolios/portfolio-1/portfolio-1.js
@@ -1,36 +1,36 @@
-const arrow_forward = document.querySelector(".carousel .arrow-right");
-const arrow_backward = document.querySelector(".carousel .arrow-left");
-const carouselImage = document.querySelector(".carousel-track img");
+  const arrow_forward = document.querySelector(".carousel .arrow-right");
+  const arrow_backward = document.querySelector(".carousel .arrow-left");
+  const carouselImage = document.querySelector(".carousel-track img");
 
-const images = [
-  "images/Slide1.PNG",
-  "images/Slide2.PNG",
-  "images/Slide3.PNG",
-  "images/Slide4.PNG",
-  "images/Slide5.PNG",
-  "images/Slide6.PNG",
-  "images/Slide7.PNG",
-  "images/Slide8.PNG",
-  "images/Slide9.PNG",
-  "images/Slide10.PNG",
-  "images/Slide11.PNG",
-];
+  const images = [
+    "images/Slide1.PNG",
+    "images/Slide2.PNG",
+    "images/Slide3.PNG",
+    "images/Slide4.PNG",
+    "images/Slide5.PNG",
+    "images/Slide6.PNG",
+    "images/Slide7.PNG",
+    "images/Slide8.PNG",
+    "images/Slide9.PNG",
+    "images/Slide10.PNG",
+    "images/Slide11.PNG",
+  ];
 
-let currentSlideIndex = 0;
+  let currentSlideIndex = 0;
 
-function updateImage() {
-  carouselImage.src = images[currentSlideIndex];
-}
+  function updateImage() {
+    carouselImage.src = images[currentSlideIndex];
+  }
 
-arrow_forward.addEventListener("click", () => {
- 
-  currentSlideIndex = (currentSlideIndex + 1) % images.length;
+  arrow_forward.addEventListener("click", () => {
+  
+    currentSlideIndex = (currentSlideIndex + 1) % images.length;
+    updateImage();
+  });
+
+  arrow_backward.addEventListener("click", () => {
+    currentSlideIndex = (currentSlideIndex - 1 + images.length) % images.length;
+    updateImage();
+  });
+
   updateImage();
-});
-
-arrow_backward.addEventListener("click", () => {
-  currentSlideIndex = (currentSlideIndex - 1 + images.length) % images.length;
-  updateImage();
-});
-
-updateImage();
